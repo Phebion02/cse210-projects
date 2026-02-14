5845b85c7b0ebaaa745f366072b910419777e21e
Phebion02 <phebionmadondo02@gmail.com>
2026-02-14 18:50:29 +0200

    program.cs

--- week05/Mindfulness/Program.cs
+++ week05/Mindfulness/Program.cs
@@ -5,5 +5,34 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Mindfulness Project.");
+    {
+        string choice = "";
+
+        while (choice != "4")
+        {
+            Console.Clear();
+            Console.WriteLine("Menu Options:");
+            Console.WriteLine("1. Start Breathing Activity");
+            Console.WriteLine("2. Start Reflecting Activity");
+            Console.WriteLine("3. Start Listing Activity");
+            Console.WriteLine("4. Quit");
+            Console.Write("Select a choice: ");
+
+            choice = Console.ReadLine();
+
+            if (choice == "1")
+            {
+                new BreathingActivity().Run();
+            }
+            else if (choice == "2")
+            {
+                new ReflectingActivity().Run();
+            }
+            else if (choice == "3")
+            {
+                new ListingActivity().Run();
+            }
+        }
     }
 }
+    }
